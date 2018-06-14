592fbcc1dcc4857c7a30c3f60f03dc2b460dc737
YOUR NAME <YOUR@EMAIL.com>
2018-06-15 00:35:40 +1000

    fixing git

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./App.css";
+import HeadNavbar from "./components/Navbar";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        < HeadNavbar />
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
+{/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">BOOBY React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p> */}
