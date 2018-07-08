4d53ff2d6ce8a5a1bc49d16919e16800d6e83958
YOUR NAME <YOUR@EMAIL.com>
2018-07-08 21:08:20 +1000

    added kickstarter and haizol logos

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -8,7 +8,8 @@ import ModalExample from "../../components/PopUpSignUp.js";
 import MainJumbotron from "../../components/Jumbotron";
 import InventionProcessDeck from "../../components/Decks";
 import ClientStories from "../../components/clientStories";
-
+import kickstarterLogo from "../../images/kickstarter-logo.png";
+import haizolLogo from "../../images/haizol-logo.png";
 // /Users/omar/Desktop/innovationDrive/innovation-drive/src/components/Form.js
 
 export default class Homepage extends Component {
@@ -34,10 +35,10 @@ export default class Homepage extends Component {
         <Container>
           <ClientStories />
         </Container>
-       <br/>
-       <br/>
+        <br />
+        <br />
         <InventionProcessDeck />
-        <Container className="YellowStyling">
+        {/* <Container className="YellowStyling">
           <Row>
             <br />
             <br />
@@ -47,6 +48,24 @@ export default class Homepage extends Component {
             <br />
             <br />
             <br />
+          </Row>
+        </Container> */}
+        <Container className="Logos">
+          <Row>
+            {/* ADD LOGOS HERE */}
+            <img
+              src={kickstarterLogo}
+              className="kickstarter-logo"
+              alt="logo"
+            />
+              <img
+              src={haizolLogo}
+              className="kickstarter-logo"
+              alt="logo"
+            />
+          </Row>
+          <Row>
+
           </Row>
         </Container>
       </div>
