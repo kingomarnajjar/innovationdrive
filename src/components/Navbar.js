42b19779cdba476692250038dceb70b66ef40080
YOUR NAME <YOUR@EMAIL.com>
2018-08-07 01:15:40 +1000

    changed css, added reactsrap navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,56 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavLink,
+} from "reactstrap";
+import logo from '../img/logo.png'
 
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
-const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
-    </div>
-  </nav>
-)
 
-export default Navbar
+export default class HeadNavbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+
+
+    });
+  }
+  render() {
+    return (
+      <div id="top">
+        <Navbar className="navbar" light expand="md" fixed={`top`}>
+          <NavbarBrand href="/">
+            <img src={logo} className="App-logo" alt="logo" style={{ height: '10vh', paddingLeft: '14px', paddingTop: '10px' }} />
+          </NavbarBrand>
+          {/* <NavbarToggler onClick={this.toggle} /> */}
+
+          <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+
+                {/* <NavLink href="https://blog.innovationdrive.co">
+                  Blog
+                </NavLink>
+                <NavLink href="https://app.innovationdrive.co">
+                  Sign In
+                </NavLink> */}
+
+              </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
